1ffd4fb01c47dd5a5cec730d42cc0d80fc20e7da
wrwangqiyuan1@outlook.com <wrwangqiyuan1@outlook.com>
2024-07-18 23:09:50 +0800

    修改ALDque.h，修改了grow函数和plusone和minusone函数，并使用这些函数修改addfirst和addlast

--- ALDeque.h
+++ ALDeque.h
@@ -2,6 +2,8 @@
 #include<iostream>
 using  namespace std;
 /*该文件使用数组来模拟双端队列*/
+/*与队列类似，因此需要有指向结尾的指针，有指向开头的指针，用记录开头索引和结尾索引的方式记录指针*/
+/*使用plusone和minusone向后移和向前移动，模拟循环*/
 template<class T>
 class ALDeque
 {
@@ -9,21 +11,17 @@ class ALDeque
 	int size;////记录队列的长度
 	int len;//记录数组的长度
 	/*队列的长度和数组的长度是不一样的，队列的起始位置不是数组的索引为0处，结束位置也不一定是数组结束位置，起始位放在中间，可以addfirst也可以addlast*/
-	int 0_position;//在数组中队列的起始位置的索引
 	/*这两个索引是相当于使用索引来模拟指针，当队列到起始位时可以使用first_prev来移动到移动到最后一个，到最后一个时同理可以使用last-next回到起点，实现循环*/
 	int first_prev;//记录队列的最后一个位置
 	int last_next;//记录队列的起始点
+	int Plusone(int index,int length);//索引往下加一个，因为是模拟队列所以跟原始的加不一样
+	int Minusone(int index);//索引往前减一个，同上
 public:
-	ALDeque() { item = new T[8](0); size = 0; 0_postition = 8 / 2; first_prev = 0, last_next = 0; len = 8; }
-	ALDeque(T mitem) { item = new T[8](0); item[0] = mitem; size = 1; 0_postiton = 8 / 2; item[0_positin] = mitem; first_prev = 0_posision + size - 1; last_next = 0_position; len = 8; }
+	ALDeque() { item = new T[8](0); size = 0; 0_postition = 8 / 2; first_prev = 4, last_next = 4; len = 8; }
 	void Addfirst(T x);//在头加
 	void Addlast(T x);//在尾巴加
 	int size() { return size; }
-	void Update_size();//更新数组的长度
-	void Update() {//更新first_prev和last_next的指向，每次操作完要更新
-		first_prev = 0_position + size - 1;
-		last_next = 0_position;
-	}
+	void Grow();//队列成长
 	T Removefirst();//移除首元素
 	T Removelast();//移除尾元素
 	T Get(int index);//得到指定索引的值
@@ -33,33 +31,25 @@ public:
 template<class T>
 void ALDeque<T>::Addfirst(T x)
 {
-	if (0_position-1>=0)//添加首元素前判断队列的首位置的索引是否往前加会超过数组的0位置
+	if (size==len-1)
 	{
-		0_position--;
-		size++;
-		Update();
-		item[0_position] = x;
+		Grow();
 	}
-	else {//是的话更新大小，再次添加；
-		Update_size();
-		Addfirst(x);
-	}
-	
+	last_next = Minusone(last_next);
+	item[last_next] = x;
+	size++;
 	return;
 }
 template<class T>
 void ALDeque<T>::Addlast(T x)
 {
-	if (size+1<len)
+	if (size==len-1)
 	{
-		size++;
-		Update();
-		item[first_prev] = x;
+		Grow();
 	}
-	else {
-		Update_size();
-		Addlast(x);
-	}
+	front_prev = Plusone(front_next,len);
+	item[front_prev] = x;
+	size++;
 	return;
 }
 template<class T>
@@ -67,11 +57,9 @@ T ALDeque<T>::Removefirst()
 {
 	if (!Isempty())///取出前判断是否为空
 	{
-		int temp = 0_position;
-		0_position++;
-		size--;
-		Update();
-		return item[0_position];
+		T temp = item[last_prev];
+		last_next = Plusone(last_next,len);
+		return temp;
 	}
 	else {
 		cout<<"链表为空" << endl;
@@ -83,22 +71,22 @@ T ALDeque<T>::Removelast()
 {
 	if (!Isempty())
 	{
-		int temp = 0_position;
-		size--;
-		Update();
-		return itme[0_position];
+		T temp = item[first_next];
+		first_prev = Minusone(first_prev);
+		return temp;
 	}
 	else
 	{
 		cout<<"链表为空" << endl;
-	}
-	
+	}	
 }
+
 template<class T>
 T ALDeque<T>::Get(int index)
 {
 	return item[0_position+index];
 }
+
 template<class T>
 void ALDeque<T>::PrintDeque()
 {
@@ -112,21 +100,46 @@ void ALDeque<T>::PrintDeque()
 	}
 	return;
 }
+
 template<class T>
-void ALDeque<T>::Update_size()
+int ALDeque<T>::Plusone(int index, int length)
 {
-	T* new_item = new T[size*3](0);//添加3倍的大小，那么向前加和向后加都可以加size大小
-	int i = size;
-	int j = 0_posiiton;
-	for (i;i<size*2;i++)
+	index %= length;
+	if (index==length-1)//往下加，当索引到最后一个时下一个位置回到数组第一个
 	{
-		new_item[i] = item[j];
-		j++;
+		return 0;
 	}
-	0_position = size;
-	Update();
-	T* temp = item;
+	return index + 1;
+}
+
+template<class T>
+int ALDeque<T>::Minusone(int index)
+{
+	if (index==0)//往前减，理由同上
+	{
+		return length-1;
+	}
+	return index - 1;
+}
+template<class T>
+void ALDeque<T>:: Grow()
+{
+	T*new_item = new T[len * 2](0);
+	T* temp=nullptr;
+	int ptr1 = last_next;
+	int ptr2 = len;
+
+	while (ptr1!=first_prev)
+	{
+		new_item[ptr2] = item[ptr1];
+		ptr1 = Plusone(ptr1,len);
+		ptr2 = Plusone(ptr2,len*2);
+	}
+	new_item[ptr2] = item[ptr1];
+	first_prev = ptr2;
+	last_next = len;
+	temp = item;
 	item = new_item;
-	delete[]new_item;
+	delete[]temp;
 	return;
 }
